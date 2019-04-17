98c825e2628a262e24df7c6dc504061eae24562a
valxska <49490453+valxska@users.noreply.github.com>
2019-04-17 14:21:41 -0600

    Base de datos

--- WindowsFormsApplication1/LogInForm.Designer.cs
+++ WindowsFormsApplication1/LogInForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace Main_Admin
 {
     partial class LogInForm
     {
@@ -39,9 +39,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Intel Clear Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(201, 336);
+            this.button1.Location = new System.Drawing.Point(226, 420);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 39);
+            this.button1.Size = new System.Drawing.Size(162, 49);
             this.button1.TabIndex = 0;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,9 +52,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Intel Clear Light", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(68, 28);
+            this.label1.Location = new System.Drawing.Point(76, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(435, 57);
+            this.label1.Size = new System.Drawing.Size(525, 69);
             this.label1.TabIndex = 1;
             this.label1.Text = "Manejo de Inventario ";
             // 
@@ -61,9 +62,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Intel Clear Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(120, 194);
+            this.label2.Location = new System.Drawing.Point(135, 242);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 27);
+            this.label2.Size = new System.Drawing.Size(88, 32);
             this.label2.TabIndex = 2;
             this.label2.Text = "WWID:";
             // 
@@ -71,39 +72,44 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Intel Clear Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(120, 261);
+            this.label3.Location = new System.Drawing.Point(135, 326);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 27);
+            this.label3.Size = new System.Drawing.Size(78, 32);
             this.label3.TabIndex = 3;
             this.label3.Text = "Clave:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(232, 194);
+            this.textBox1.Location = new System.Drawing.Point(261, 242);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(192, 27);
+            this.textBox1.Size = new System.Drawing.Size(216, 33);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(232, 261);
+            this.textBox2.Location = new System.Drawing.Point(261, 326);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(192, 27);
+            this.textBox2.Size = new System.Drawing.Size(216, 33);
             this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // LogInForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 450);
+            this.ClientSize = new System.Drawing.Size(620, 562);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "LogInForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace WindowsFormsApplication1
 {
     static class Program
@@ -14,6 +17,36 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
+
+
+            string connStr = "server=localhost;user id=root;persistsecurityinfo=True;database=inventario_ic";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Fm_Lector());
